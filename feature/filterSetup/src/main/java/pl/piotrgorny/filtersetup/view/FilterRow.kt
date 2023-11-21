b196edf8dc6ac79b8c15be3559cfcf8764d7749c
plicios <piotr.r.gorny@gmail.com>
2023-11-21 10:34:51 +0100

    clean

--- feature/filterSetup/src/main/java/pl/piotrgorny/filtersetup/view/FilterRow.kt
+++ feature/filterSetup/src/main/java/pl/piotrgorny/filtersetup/view/FilterRow.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.joda.time.LocalDate
 import pl.piotrgorny.filtersetup.R
 import pl.piotrgorny.model.Filter
 
